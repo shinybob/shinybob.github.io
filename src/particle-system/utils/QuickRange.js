edda620277e8ca8bcf73c44830da28d95c94777c
robthurston <rob.thurston@gl05585m.gamesys.corp>
2017-02-14 13:05:20 +0000

    cleaning up

--- src/particle-system/utils/QuickRange.js
+++
@@ -1,9 +0,0 @@
-var QuickRange = {};
-
-QuickRange.range = function(min, max, round) {
-    var value = min + (Math.random() * (max - min));
-    if(round) value = Math.round(value);
-    return value;
-};
-
-module.exports = QuickRange;
